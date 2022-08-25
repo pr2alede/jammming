a7179676e481806e69994742285f330272a6dbb0
pr2alede <aled_evans_elli@yahoo.co.uk>
2022-08-25 13:50:32 +0100

    Step 61 - Change playlist name functionality been added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component{
     };
     this.addTrack=this.addTrack.bind(this);
     this.removeTrack=this.removeTrack.bind(this);
+    this.updatePlaylistName=this.updatePlaylistName.bind(this);
   }
   removeTrack(track){
     let tracks = this.state.playlistTracks;
@@ -35,6 +36,10 @@ class App extends React.Component{
     this.setState({playlistTracks:tracks});
 
   }
+
+  updatePlaylistName(name){
+    this.setState({playlistName:name})
+  }
   render(){
     return (
     <div>
@@ -49,7 +54,8 @@ class App extends React.Component{
       
       <Playlist playlistName= {this.state.playlistName} 
       playlistTracks = {this.state.playlistTracks} 
-      onRemove={this.removeTrack} />
+      onRemove={this.removeTrack}
+      onNameChange ={this.updatePlaylistName} />
        
     </div>
   </div>
